1c2436642ce4d59fd09117f1d9284956cc9dd811
xuyingcool <32608685+xuyingcool@users.noreply.github.com>
2020-03-02 19:03:11 +0800

    Update Hero.java

--- src/search/Hero.java
+++ src/search/Hero.java
@@ -3,7 +3,7 @@ package search;
 public class Hero {
     public static String copyright;
     static {
-        System.out.println("初始化");
+        System.out.println("初始化2");
         copyright = "xy";
     }
 }
